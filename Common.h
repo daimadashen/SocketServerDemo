627fd27cb37b5b4b21f46b54492945ae0da514c3
zhangfeng <zhangfeng@nanguiyu.com>
2019-08-02 17:46:01 +0800

    Initial commit

---
+++ Common.h
@@ -0,0 +1,24 @@
+//
+// Created by zhangfeng on 2019/8/2.
+//
+
+#ifndef SOCKETSERVERDEMOE_COMMON_H
+#define SOCKETSERVERDEMOE_COMMON_H
+
+#include <iostream>
+#include <list>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SERVER_IP "127.0.0.1"
+
+#endif //SOCKETSERVERDEMOE_COMMON_H
